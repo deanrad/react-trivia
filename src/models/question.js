b36d2932d8bcc7be2c47ef991eacafa403d9bd49
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-05-26 04:26:11 -0500

    Make the filename lowercase through Sublime- it will not lowercase your OSX file - haha\!

--- src/models/question.js
+++ src/models/question.js
@@ -4,9 +4,7 @@ import questions from '../../react-trivia-questions'
 export default class Question {
   static nextQuestion(current) {
     let {prompt} = current || {}
-    console.log('DRDR' +prompt)
     let idx = questions.findIndex(q => q.prompt==prompt)
-    console.log('DRDR2' + idx)
     let nextIdx = idx + 1
     return questions[nextIdx]
   }
