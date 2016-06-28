84ee8c120c2a9bbad59fac8ccb6de61e36ce8ede
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-06-28 09:31:03 -0500

    State gets shredded

--- client/src/bindActions.js
+++ client/src/bindActions.js
@@ -1,4 +1,6 @@
 import {assignAll} from 'redux-act'
 import Actions from './actions'
 
-export default (store) => assignAll(Actions, store)
+export default (store) => {
+  assignAll(Actions, [console.log.bind(console, 'Foo: '), store])
+}
