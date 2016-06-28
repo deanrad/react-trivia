81a1f86ad936dce2f9e9c55eba9059c5634fef4c
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-06-28 15:35:20 -0500

    Redux devtools works again, huzzah!
    Because I'm not using middleware, obvy (wait, wat?)

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,11 @@ import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducer'
 
 // https://github.com/gaearon/redux-devtools/issues/274, fixed in 1.4.*
-// const storeCreator = window.devToolsExtension ?
-//     window.devToolsExtension()(createStoreWithMiddleware) :
-//     createStoreWithMiddleware
+const storeMaker = window.devToolsExtension ?
+    window.devToolsExtension()(createStore) :
+    createStore
 
-const store = createStore(reducer)
+const store = storeMaker(reducer)
 
 window.store = store
 export default store
